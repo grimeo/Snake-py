2b605a7b6ad65056f8bee019f61b9e436f6bee8f
Romeo <romeogescolano@gmail.com>
2022-11-21 21:12:47 +0800

    finish: collision detector

--- index.py
+++ index.py
@@ -5,10 +5,10 @@ GAME_WIDTH = 700
 GAME_HEIGHT = 700
 SPEED = 50
 SPACE_SIZE = 50
-BODY_PARTS = 3
-SNAKE_COLOR = "green"
-FOOD_COLOR = "red"
-BACKGROUND_COLOR = "black"
+BODY_PARTS = 10
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
 
 
 class Snake:
@@ -22,13 +22,13 @@ class Snake:
             self.coordinates.append([0, 0])
              
         for x, y in self.coordinates:
-            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tags="snake")
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
             self.squares.append(square)
              
 class Food:
     def __init__(self):
-        x = random.randint(0, (GAME_WIDTH / SPACE_SIZE) * SPACE_SIZE )
-        y = random.randint(0, (GAME_HEIGHT / SPACE_SIZE) * SPACE_SIZE )
+        x = random.randint(0, (GAME_WIDTH / SPACE_SIZE)-1) * SPACE_SIZE 
+        y = random.randint(0, (GAME_HEIGHT / SPACE_SIZE)-1) * SPACE_SIZE 
         
         self.coordinates = [x, y]
         
@@ -52,20 +52,68 @@ def next_turn(snake, food):
     
     snake.squares.insert(0, square)
     
-    del snake.coordinates[-1]
+    if x == food.coordinates[0] and y == food.coordinates[1]:
+        
+        global score
+        
+        score += 1
+        
+        label.config(text = "Score:{}".format(score))
+        
+        canvas.delete("food")
+        
+        food = Food()
     
-    canvas.delete(snake.squares[-1])
-    
-    del snake.squares[-1]
-    
-    window.after(SPEED, next_turn, snake, food)
+    else:
+        del snake.coordinates[-1]
+        
+        canvas.delete(snake.squares[-1])
+        
+        del snake.squares[-1]
+        
+    if check_collisions(snake):
+        game_over()
+    else:
+        window.after(SPEED, next_turn, snake, food)
 
 def change_direction(new_direction):
-    pass
+    
+    global direction
+    
+    if new_direction == 'left':
+        if direction != 'right':
+            direction = new_direction
+    if new_direction == 'right':
+        if direction != 'left':
+            direction = new_direction
+    if new_direction == 'up':
+        if direction != 'down':
+            direction = new_direction
+    if new_direction == 'down':
+        if direction != 'up':
+            direction = new_direction
 
-def check_collision():
-    pass
 
+
+def check_collisions(snake):
+    
+    x, y = snake.coordinates[0]
+    
+    if x < 0 or x >= GAME_WIDTH:
+        print("GAME OVER")
+        return True
+    
+    elif y < 0 or y >= GAME_HEIGHT:
+        print("GAME OVER")
+        return True
+    
+    for body_part in snake.coordinates[1:]:
+        if x == body_part[0] and y == body_part[1]:
+            print("GAME OVER")
+            return True
+    
+    return False
+        
 def game_over():
     pass
 
@@ -78,7 +126,7 @@ score = 0
 direction = "down"
 
 
-label = Label(window, text="Score:{}".format(score), font=("consolas", 40))
+label = Label(window, text="Score:{}".format(score), font=("consolas", 20))
 label.pack()
 
 canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
@@ -93,6 +141,11 @@ screen_height = window.winfo_screenheight()
 x = int((screen_width/2) - (window_width/2))
 y = int((screen_height/2) - (window_height/2))
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+window.bind('<Left>', lambda event: change_direction('left'))
+window.bind('<Right>', lambda event: change_direction('right'))
+window.bind('<Up>', lambda event: change_direction('up'))
+window.bind('<Down>', lambda event: change_direction('down'))
 
 snake = Snake()
 food = Food()
